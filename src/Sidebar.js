7992e1eae9de7bdf8954471e94233ab2655ce99d
Edyino <edyino@MacBook-Air-di-Valeria.local>
2022-09-11 16:06:38 +0200

    Add menu

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,21 +1,45 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import MenuIcon from "./img/menu.svg";
+import CloseIcon from "./img/close.svg";
 
 const Sidebar = () => {
+  const [showNavBar, setShowNavbar] = useState(false);
+
   return (
-    <nav className="basis-72 pt-14">
-      <Link className="block p-2 pl-8" to="/prologo">
-        Prologo
-      </Link>
-      <Link className="block p-2 pl-8" to="/bibliografia">
-        Bibliografía del autor
-      </Link>
-      <Link className="block p-2 pl-8" to="/fotografias">
-        Fotografias
-      </Link>
-      <Link className="block p-2 pl-8" to="/poesia">
-        Poesia
-      </Link>
-    </nav>
+    <>
+      {showNavBar ? (
+        <div className="flex flex-col pt-4">
+          <div className="self-end">
+            <button onClick={() => setShowNavbar(false)}>
+              <img className="max-h-6" src={CloseIcon} alt="" />
+            </button>
+          </div>
+          <nav className="basis-72 pt-4">
+            <Link className="block p-2 pl-8" to="/prologo">
+              Prologo
+            </Link>
+            <Link className="block p-2 pl-8" to="/bibliografia">
+              Bibliografía del autor
+            </Link>
+            <Link className="block p-2 pl-8" to="/fotografias">
+              Fotografias
+            </Link>
+            <Link className="block p-2 pl-8" to="/poesia">
+              Poesia
+            </Link>
+          </nav>
+        </div>
+      ) : (
+        <button onClick={() => setShowNavbar(true)}>
+          <img
+            className="max-h-8 absolute top-4 left-4"
+            src={MenuIcon}
+            alt=""
+          />
+        </button>
+      )}
+    </>
   );
 };
 
